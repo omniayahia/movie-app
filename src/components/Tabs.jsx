698770ca1f41d2717c7b9adb54857d523fb160f7
omnia yahia <omnia.yehia@espace.com.eg>
2020-08-22 02:57:53 +0200

    PWA

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import Card from './Card';
 
 export default class Tabs extends Component
@@ -21,13 +22,13 @@ export default class Tabs extends Component
         <div className="shadow-sm bg-white">
           <ul className="container nav nav-pills mb-4 pt-1 justify-content-end " id="pills-tab" role="tablist">
           <li className="nav-item">
-            <a className="nav-link active mx-2"  onClick = {()=>this.changeMovieCategory("popular")}  id="pills-popular-tab"  data-toggle="pill" href="#pills-popular" role="tab" aria-controls="pills-popular" aria-selected="true" >Popular</a>
+            <Link className="nav-link active mx-2"  onClick = {()=>this.changeMovieCategory("popular")}  id="pills-popular-tab"  data-toggle="pill" to="/pills-popular" role="tab" aria-controls="pills-popular" aria-selected="true" >Popular</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link mx-2" onClick = {()=>this.changeMovieCategory("top_rated")} id="pills-topRated-tab" data-toggle="pill" href="#pills-topRated" role="tab" aria-controls="pills-topRated" aria-selected="false">Top Rated</a>
+            <Link className="nav-link mx-2" onClick = {()=>this.changeMovieCategory("top_rated")} id="pills-topRated-tab" data-toggle="pill" href="/pills-topRated" role="tab" aria-controls="pills-topRated" aria-selected="false">Top Rated</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" onClick = {()=>this.changeMovieCategory("upcoming")} id="pills-upcoming-tab" data-toggle="pill" href="#pills-upcoming" role="tab" aria-controls="pills-upcoming" aria-selected="false">Upcoming</a>
+            <Link className="nav-link" onClick = {()=>this.changeMovieCategory("upcoming")} id="pills-upcoming-tab" data-toggle="pill" href="/pills-upcoming" role="tab" aria-controls="pills-upcoming" aria-selected="false">Upcoming</Link>
           </li>
         </ul> </div>
         <div className="clearfix"></div>
